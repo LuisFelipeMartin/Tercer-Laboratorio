86a9ee76dacfa57a94b3be7f89c854be1c48e1cf
USUARIO <USUARIO@DESKTOP-MUK1R6H>
2023-09-11 20:38:52 -0500

    Segundo Commit :0

--- src/Semaforo_GUI.java
+++ src/Semaforo_GUI.java
@@ -1,12 +1,16 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.ImageIcon;
+import javax.swing.Timer;
 public class Semaforo_GUI extends javax.swing.JFrame 
 {
-    String estado;
-    int trafico;
+    ImageIcon[] sprite=new ImageIcon[8];
     public Semaforo_GUI() 
     {
+        peatonEspera.start();
         initComponents();
     }
-    
+ 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -18,12 +22,12 @@ public class Semaforo_GUI extends javax.swing.JFrame
         Luz_Roja = new javax.swing.JLabel();
         Titulo_Trafico = new javax.swing.JLabel();
         Slider_Trafico = new javax.swing.JSlider();
-        Animacion_Peaton = new javax.swing.JLabel();
         Bot_Paso = new javax.swing.JButton();
         Titulo_Paso = new javax.swing.JLabel();
         Tex_Rojo = new javax.swing.JTextField();
         Tex_Amarillo = new javax.swing.JTextField();
         Tex_Verde = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setType(java.awt.Window.Type.UTILITY);
@@ -35,7 +39,7 @@ public class Semaforo_GUI extends javax.swing.JFrame
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel1.setText("Semaforo medio feo :0");
 
-        Luz_Verde.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/Luz verde apagada.png"))); // NOI18N
+        Luz_Verde.setIcon(new javax.swing.ImageIcon("C:\\Users\\USUARIO\\Desktop\\Universidad\\2023 - 2 + Laboratorio de programacion 3 + Mec lab 3\\Trabajos\\Segundo corte\\2023 - 2 + Informe 03 + Semaforo\\Luz verde apagada.png")); // NOI18N
 
         Luz_Amarilla.setIcon(new javax.swing.ImageIcon("C:\\Users\\USUARIO\\Desktop\\Luz amarilla apagada.png")); // NOI18N
 
@@ -47,8 +51,6 @@ public class Semaforo_GUI extends javax.swing.JFrame
         Titulo_Trafico.setText("Trafico");
 
         Slider_Trafico.setBackground(new java.awt.Color(51, 51, 51));
-
-        Animacion_Peaton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/Esperar_1.png"))); // NOI18N
 
         Bot_Paso.setFont(new java.awt.Font("Segoe UI", 1, 20)); // NOI18N
         Bot_Paso.setText("Dar Paso");
@@ -89,6 +91,8 @@ public class Semaforo_GUI extends javax.swing.JFrame
         Tex_Verde.setText("00:00");
         Tex_Verde.setBorder(null);
 
+        jLabel2.setText("jLabel2");
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -96,43 +100,36 @@ public class Semaforo_GUI extends javax.swing.JFrame
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(20, 20, 20)
+                        .addGap(65, 65, 65)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(Slider_Trafico, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(Titulo_Trafico, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addGap(65, 65, 65)
-                                .addComponent(Tex_Rojo, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addGap(65, 65, 65)
-                                .addComponent(Luz_Roja)))
+                            .addComponent(Tex_Rojo, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(Luz_Roja))
                         .addGap(10, 10, 10)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(Luz_Amarilla)
                             .addComponent(Tex_Amarillo, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(10, 10, 10)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(Tex_Verde, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(Luz_Verde))
+                        .addGap(0, 45, Short.MAX_VALUE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(20, 20, 20)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addGap(10, 10, 10)
-                                .addComponent(Tex_Verde, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addGap(10, 10, 10)
-                                .addComponent(Luz_Verde)))
-                        .addGap(0, 0, Short.MAX_VALUE)))
+                                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 69, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(45, 45, 45)
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                    .addComponent(Bot_Paso, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                    .addComponent(Titulo_Paso, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 32, Short.MAX_VALUE))
+                            .addComponent(Slider_Trafico, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(Titulo_Trafico, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
                 .addGap(20, 20, 20))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jLabel1)
                 .addGap(90, 90, 90))
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(20, 20, 20)
-                .addComponent(Animacion_Peaton)
-                .addGap(50, 50, 50)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(Bot_Paso, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(Titulo_Paso, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addContainerGap(50, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -152,16 +149,14 @@ public class Semaforo_GUI extends javax.swing.JFrame
                 .addComponent(Titulo_Trafico, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(5, 5, 5)
                 .addComponent(Slider_Trafico, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(20, 20, 20)
+                .addGap(10, 10, 10)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(Animacion_Peaton)
-                        .addContainerGap(24, Short.MAX_VALUE))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
                         .addComponent(Bot_Paso)
-                        .addGap(5, 5, 5)
-                        .addComponent(Titulo_Paso, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(0, 0, Short.MAX_VALUE))))
+                        .addGap(10, 10, 10)
+                        .addComponent(Titulo_Paso, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 78, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(20, 20, 20))
         );
 
         getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 300, 300));
@@ -170,7 +165,8 @@ public class Semaforo_GUI extends javax.swing.JFrame
     }// </editor-fold>//GEN-END:initComponents
 
     private void Bot_PasoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Bot_PasoActionPerformed
-        // TODO add your handling code here:
+        peatonEspera.stop();
+        peatonMovimiento.start();
     }//GEN-LAST:event_Bot_PasoActionPerformed
 
     private void Tex_RojoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Tex_RojoActionPerformed
@@ -178,7 +174,6 @@ public class Semaforo_GUI extends javax.swing.JFrame
     }//GEN-LAST:event_Tex_RojoActionPerformed
         
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel Animacion_Peaton;
     private javax.swing.JButton Bot_Paso;
     private javax.swing.JLabel Luz_Amarilla;
     private javax.swing.JLabel Luz_Roja;
@@ -190,8 +185,42 @@ public class Semaforo_GUI extends javax.swing.JFrame
     private javax.swing.JLabel Titulo_Paso;
     private javax.swing.JLabel Titulo_Trafico;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
+    int i=0;
+    Timer peatonEspera = new Timer(200, new ActionListener()
+    {
+        public void actionPerformed(ActionEvent e) 
+        {
+            if(i<5)
+            {
+                sprite[i]=new ImageIcon("Esperar_"+(i+1)+".png");
+                jLabel2.setIcon(sprite[i]);
+                i++;
+            }
+            else
+            {
+                i=0;
+            }
+        }
+    });
+    Timer peatonMovimiento = new Timer(80, new ActionListener()
+    {
+        public void actionPerformed(ActionEvent e) 
+        {
+            if(i<3)
+            {
+                sprite[i]=new ImageIcon("Caminar_"+(i+1)+".png");
+                jLabel2.setIcon(sprite[i]);
+                i++;
+            }
+            else
+            {
+                i=0;
+            }
+        }
+    });
     public static void main(String args[]) 
     {
         
